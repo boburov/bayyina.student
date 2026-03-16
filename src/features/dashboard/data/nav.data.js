48ce9ab8bc842c867068acf794e45e4224af9fe6
yaxyobekuz <pubgn9642@gmail.com>
2026-03-16 07:31:56 +0500

    feat: update gradient colors for top navigation items in nav.data.js

--- src/features/dashboard/data/nav.data.js
+++ src/features/dashboard/data/nav.data.js
@@ -7,16 +7,16 @@ const topNavItems = [
     label: "Jarimalar",
     description: "Jarimalaringizni ko'rish uchun",
     icon: TriangleAlert,
-    gradientFrom: "from-orange-400",
-    gradientTo: "to-orange-700",
+    gradientFrom: "from-red-400",
+    gradientTo: "to-red-700",
   },
   {
     to: "/games",
     label: "O'yinlar",
     description: "Aqlingizni chiniqtirish uchun",
     icon: Gamepad2,
-    gradientFrom: "from-emerald-400",
-    gradientTo: "to-green-700",
+    gradientFrom: "from-orange-400",
+    gradientTo: "to-orange-700",
   },
 ];
 
