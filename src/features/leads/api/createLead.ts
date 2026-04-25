bf6ae0162a5376bb73c59bc06e3463007a11756f
boburov <boburovdev@gmail.com>
2026-04-25 15:34:49 +0500

    production test

--- src/features/leads/api/createLead.ts
+++ src/features/leads/api/createLead.ts
@@ -11,7 +11,7 @@ export interface LeadPayload {
 }
 
 export const createLead = async (data: LeadPayload): Promise<void> => {
-  await axios.post('http://156.67.29.62:4000/api/leads', data, {
+  await axios.post('http://api.bayyina.org.uz/api/leads', data, {
     headers: { 'Content-Type': 'application/json' },
   });
 };
